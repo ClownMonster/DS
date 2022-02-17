f6f76423a65128fb55022ba0f4da6de117ee46d1
Clown Monster <mk6386223@gmail.com>
2022-02-17 23:48:47 +0000

    t

--- one.py
+++ one.py
@@ -1,70 +1,100 @@
-from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
-import sklearn.metrics as met 
-from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 
 
-cancer = pd.read_csv('breast-cancer.data',sep=",") 
-df = pd.DataFrame(cancer)
-print(df.head())
+df = pd.read_csv('/content/iris.csv')
+df.head()
+
+df.describe()
 
 
-le = preprocessing.LabelEncoder()
-id3_model = DecisionTreeClassifier(criterion = 'entropy') 
-cart_model = DecisionTreeClassifier(criterion = 'gini') 
-print(id3_model)
-print(cart_model)
+df.info()
 
 
-X = df.iloc[:,:-1].values
-Y = df.iloc[:,-1].values
-for i in range(9): 
-    le.fit(list(set(df.iloc[:,i].values))) 
-    a =[]
-    for j in range(len(X)): 
-        a.append(X[j][i])
-    a = le.transform(a)
-    for j in range(len(X)): 
-        X[j][i] = a[j]
-le.fit(list(set(df.iloc[:,-1].values))) 
-a =[]
+df['Class']=df['Class'].map({
+    'Iris-setosa':0,
+    'Iris-versicolor':1,
+    'Iris-virginica':2
+    
+})
 
-for j in range(len(X)): 
-    a.append(Y[j])
-a = le.transform(a)
-for j in range(len(X)): 
-    Y[j] = a[j]
-print(X)  
-print(Y) 
-Y=Y.astype('int')
-Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size =int(0.1*len(Y)), random_state = 1)
-print(Xtrain) 
-print(Ytrain)
+
+df.head(5)
 
 
 
-
-id3_model_trained = id3_model.fit(Xtrain,Ytrain) 
-cart_model_trained = cart_model.fit(Xtrain,Ytrain) 
-Ypredict_cart = cart_model_trained.predict(Xtest) 
-Ypredict_id3 = id3_model_trained.predict(Xtest)
+X = df[['Sepal_Length', ' Sepal_Width', ' Petal_Length', ' Petal_Width']]
+X
 
 
+y=df[['Class']]
+y
 
 
-print("ID3 Tree Metrics")
-print("Accuracy = ", met.accuracy_score(Ytest, Ypredict_id3))
-print("Error Rate = ",1-met.accuracy_score(Ytest, Ypredict_id3))
-print("Recall = ",met.recall_score(Ytest, Ypredict_id3,average ='weighted')) 
-print("Precision = ",met.precision_score(Ytest,Ypredict_id3,average = 'weighted')) 
-print("F-Measure = ",met.f1_score(Ytest, Ypredict_id3,average = 'weighted'))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
 
 
 
-print("CART Tree Metrics")
-print("Accuracy = ", met.accuracy_score(Ytest, Ypredict_cart))
-print("Error Rate = ",1-met.accuracy_score(Ytest, Ypredict_cart))
-print("Recall = ",met.recall_score(Ytest, Ypredict_cart,average ='weighted'))
-print("Precision = ",met.precision_score(Ytest,Ypredict_cart,average = 'weighted'))
+from sklearn.tree import DecisionTreeClassifier
+id3 = DecisionTreeClassifier(criterion='entropy')
+id3_model = id3.fit(X_train, y_train)
 
+
+y_id3 = id3_model.predict(X_test)
+y_id3
+
+
+
+accuracy = accuracy_score(y_test, y_id3)
+recall = recall_score(y_test, y_id3, average='micro')
+precision = precision_score(y_test, y_id3, average='micro')
+f1 = f1_score(y_test, y_id3, average='micro')
+print("Accuracy:", accuracy)
+print("Error Rate:", 1.0-accuracy)
+print("Recall:", recall)
+print("Precision:", precision)
+print("F1 Score:", f1)
+
+
+
+from sklearn.metrics import classification_report,confusion_matrix
+cm=confusion_matrix(y_test,y_id3)
+cm
+
+
+print(classification_report(y_test,y_id3))
+
+
+
+
+cart = DecisionTreeClassifier(criterion='gini')
+cart_model = cart.fit(X_train, y_train)
+y_cart = cart_model.predict(X_test)
+y_cart
+
+
+
+accuracy = accuracy_score(y_test, y_cart)
+recall = recall_score(y_test, y_cart, average='micro')
+precision = precision_score(y_test, y_cart, average='micro')
+f1 = f1_score(y_test, y_cart, average='micro')
+print("Accuracy:", accuracy)
+print("Error Rate:", 1.0-accuracy)
+print("Recall:", recall)
+print("Precision:", precision)
+print("F1 Score:", f1)
+
+
+
+from sklearn.metrics import classification_report,confusion_matrix
+cm=confusion_matrix(y_test,y_cart)
+cm
+
+
+
+print(classification_report(y_test,y_id3))
